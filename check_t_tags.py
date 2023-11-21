fd0082de72cd5b433da41ea0c2057b7ad7468861
Suleimanov Denis <denis@suleimanov.pro>
2023-11-21 15:06:53 +0400

    Code review suggestions

--- check_t_tags.py
+++ check_t_tags.py
@@ -6,9 +6,12 @@ import checker
 directories_paths = ["./theme/sections",
                      "./theme/snippets"]
 locale_path = "./theme/locales/en.default.json"
-ch = checker.Checker(locale_path, directories_paths)
+ch = checker.Checker(locale_path, directories_paths) #the GOD object... maybe it's better to split it into smaller objects: checker, parser, liquid part.. and pass data to methods?
 
 ch.run()
 
 if ch.has_unused_keys:
     exit("Found unused translation keys!")
+
+#also: try to add tests. You'll see how it's hard to test this code because of the GOD object
+#alse 2: think about error handling.
