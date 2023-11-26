134cd6830e1bf756f46f66c672cb7cc11738c9be
paveltovchigrechko <pavel.tovchigrecko.1988@gmail.com>
2023-11-26 11:09:45 +0100

    Revert changes

--- check_t_tags.py
+++ check_t_tags.py
@@ -3,9 +3,9 @@ from sys import exit
 import checker
 
 # Initial setup
-directories_paths = ["../optimum/theme/sections",
-                     "../optimum/theme/snippets"]
-locale_path = "../optimum/theme/locales/en.default.json"
+directories_paths = ["./theme/sections",
+                     "./theme/snippets"]
+locale_path = "./theme/locales/en.default.json"
 ch = checker.Checker(locale_path, directories_paths)
 
 ch.run()
